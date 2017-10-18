5d6115710f72277704ea8d422eca75ada9b1db7b
zaragozamartin91 <zaragozamartin91@gmail.com>
2017-10-18 20:07:32 -0300

    Se hicieron vistas de ejemplo

--- routes/routes.js
+++ routes/routes.js
@@ -11,12 +11,24 @@ router.get('/', (req, res) => {
 /* GET home page. */
 router.get(config.mainPath, (req, res) => {
     logger.debug('HEEEYYY');
-    res.render('index', { title: `Belu's bakery` });
+    res.render('index', { title: `Belu's bakery`, selected:'main' });
 });
 
 router.post('/logout', (req, res) => {
     res.clearCookie('token');
     res.redirect(config.mainPath);
+});
+
+router.get('/recipes', (req, res) => {
+    res.render('recipes', { title: `Belu's bakery` , selected:'recipes' });
+});
+
+router.get('/contact', (req, res) => {
+    res.render('contact', { title: `Belu's bakery` , selected:'contact' });
+});
+
+router.get('/admin', (req, res) => {
+    res.render('admin', { title: `Belu's bakery` , selected:'admin' });
 });
 
 module.exports = router;
