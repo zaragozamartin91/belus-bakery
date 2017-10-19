021b6a1d3f0410e56e347e38c0ecc90011704251
zaragozamartin91 <zaragozamartin91@gmail.com>
2017-10-18 21:47:34 -0300

    Se arreglaron estilos. Se quitaron los sass

--- routes/routes.js
+++ routes/routes.js
@@ -5,6 +5,8 @@ const config = require('../config/main-config');
 const basicAuthParser = require('../utils/basic-auth-parser');
 const logger = require('log4js').getLogger();
 
+const title = `Belu's bakery`;
+
 router.get('/', (req, res) => {
     res.redirect(config.mainPath);
 });
@@ -12,7 +14,7 @@ router.get('/', (req, res) => {
 /* GET home page. */
 router.get(config.mainPath, (req, res) => {
     logger.debug('HEEEYYY');
-    res.render('index', { title: `Belu's bakery`, selected: 'main' });
+    res.render('index', { title, selected: 'main' });
 });
 
 router.post('/logout', (req, res) => {
@@ -21,22 +23,22 @@ router.post('/logout', (req, res) => {
 });
 
 router.get('/recipes', (req, res) => {
-    res.render('recipes', { title: `Belu's bakery`, selected: 'recipes' });
+    res.render('recipes', { title, selected: 'recipes' });
 });
 
 router.get('/contact', (req, res) => {
-    res.render('contact', { title: `Belu's bakery`, selected: 'contact' });
+    res.render('contact', { title, selected: 'contact' });
 });
 
 router.get('/admin', (req, res) => {
-    if (req.session.ok) return res.render('admin', { title: `Belu's bakery`, selected: 'admin' });
+    if (req.session.ok) return res.render('admin', { title, selected: 'admin' });
 
     const basicAuth = basicAuthParser.parse(req);
     console.log(basicAuth);
     const { user, pass } = basicAuth;
     if ('belen' == user && 'belen' == pass) {
         req.session.ok = true;
-        return res.render('admin', { title: `Belu's bakery`, selected: 'admin' });
+        return res.render('admin', { title, selected: 'admin' });
     }
 
     res.status(401);
